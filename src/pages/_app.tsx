1fefbf691625df53d3a810a54b9f50715d85d29e
anthdm <cryptoanthdm@gmail.com>
2022-08-11 10:02:21 +0200

    gtm v2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Layout from "components/Layout"
 import { EthereumProvider } from "hooks/useEthereum"
 import { NextSeo } from "next-seo"
 import Script from "next/script"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const tagManagerArgs = {
@@ -13,21 +14,17 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
-        async
         src="https://www.googletagmanager.com/gtag/js?id=G-MDNK0T4LHK"
-      ></Script>
-      <Script
-        id="gtm"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+        strategy="afterInteractive"
+      />
+      <Script id="gtm" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-            gtag('config', 'G-MDNK0T4LHK');
-            `
-        }}
-      ></Script>
+          gtag('config', 'G-MDNK0T4LHK');
+        `}
+      </Script>
       <NextSeo
         title="Don't get scammed!"
         description="An educational and interactive platform interacting with cryptocurrencies"
